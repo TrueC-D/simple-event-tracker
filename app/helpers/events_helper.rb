ff7e4807b52d7be794f4e03560ff0a35a09e74d1
TrueC-D <aletheacary@gmail.com>
2021-05-15 04:31:19 -0700

    attending_events new

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,14 +1,19 @@
 module EventsHelper
+    def future_date?
+        @event.datetime > DateTime.now
+    end
+
     def custom_show_form
         if @event.datetime > DateTime.now
             if admin?
                 render 'events/admin_event_links'
             else
-                user_is_attending = @event.attending_events.find_by(user_id: current_user)
-                if user_is_attending
-                    button_to 'Cancel Booking', attending_events_path, method: 'delete'
+                
+                attending = @event.attending_events.find(user_id: current_user)
+                if attending
+                    button_to 'Cancel Booking', event_attending_event_path(@event, attending), method: 'delete'
                 else
-                    render 'events/new_attendee'
+                    button_to "Create Booking", event_attending_events_path(@event)
                 end
             end
         else
